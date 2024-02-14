9622ae13f81764d13c41566d605ff4b039ee60a4
4v0n <navodhk@hotmail.com>
2024-02-14 17:32:39 +0000

    Quit with q

--- src/kilo.c
+++ src/kilo.c
@@ -1,17 +1,19 @@
 #include <unistd.h>
 
 int main() {
+  /* 
+  Read keypresses from user 
 
-    /* 
-    Read keypresses from user 
+  When running, the terminal is hooked to the standard input
+  Keyboard inputs will be read into the c variable
+  */
+  char c;
+  /*
+    read() reads 1 byte from the standard input into the variable, c until there are no more bytes to read
+    read() returns the number of bytes read, and will return 0 when it reaches the end of a file
+    This while loop exits when c is 'q', quitting the program
+  */
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 
-    When running, the terminal is hooked to the standard input
-    Keyboard inputs will be read into the c variable
-    */
-    char c;
-    // read() reads 1 byte from the standard input into the variable, c until there are no more bytes to read
-    // read() returns the number of bytes read, and will return 0 when it reaches the end of a file
-    while (read(STDIN_FILENO, &c, 1) == 1);
-
-    return 0;
+  return 0;
 }
