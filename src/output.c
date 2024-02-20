38f4676d3ba76a9c57915d000cba156887497603
4v0n <navodhk@hotmail.com>
2024-02-20 18:04:35 +0000

    Draw tilde column on startup

--- src/output.c
+++ src/output.c
@@ -1,7 +1,21 @@
 #include <unistd.h>
 
+// Draw a column of tildes on the left side of the screen
+void editorDrawRows()
+{
+  int y;
+  for (y = 0; y < 24; y++) { // draw 24 rows (placeholder for size of terminal)
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+// Draws each row of the buffer of text being edited
 void editorRefreshScreen()
 {
   write(STDOUT_FILENO, "\x1b[2J", 4); // clear terminal
   write(STDERR_FILENO, "\x1b[H", 3); // reposition cursor
+
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
 }
