fd25200d015bd8161ba7f841f52632e3f88723b7
4v0n <navodhk@hotmail.com>
2024-03-04 19:13:44 +0000

    Restore highlighting after search

--- src/search.c
+++ src/search.c
@@ -5,9 +5,19 @@
 #include <terminal.h>
 
 void editorFindCallback(char *query, int key) {
+  struct editorConfig *E = getEditorConfig();
+
   static int last_match = -1;
   static int direction = 1;
 
+  static int saved_hl_line;
+  static char *saved_hl = NULL;
+
+  if (saved_hl) {
+    memcpy(E->row[saved_hl_line].hl, saved_hl, E->row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
 
   if (key == '\r' || key == '\x1b') {
     last_match = -1;
@@ -21,8 +31,6 @@ void editorFindCallback(char *query, int key) {
     last_match = -1;
     direction = 1;
   }
-
-  struct editorConfig *E = getEditorConfig();
 
   if (last_match == -1) direction = 1;
   int current = last_match;
@@ -40,6 +48,9 @@ void editorFindCallback(char *query, int key) {
       E->cx = editorRowRxToCx(row, match - row->render);
       E->rowoff = E->numrows;
 
+      saved_hl_line = current;
+      saved_hl = malloc(row->rsize);
+      memcpy(saved_hl, row->hl, row->rsize);
       memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
