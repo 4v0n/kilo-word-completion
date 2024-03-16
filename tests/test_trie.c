ee36bb59f3383b52a08c1f4ef966b2b9f6d0b324
4v0n <navodhk@hotmail.com>
2024-03-16 14:18:51 +0000

    Test single word insertion

--- tests/test_trie.c
+++ tests/test_trie.c
@@ -12,8 +12,22 @@ void test_getNode() {
     CU_ASSERT_EQUAL(node->children[i], NULL);
 }
 
+// Tests single word insertion
+void test_insert_single_word() {
+  TrieNode *node = getNode();
+  insert(node, "app", 3);
+
+  // subtract index a due to offset
+  CU_ASSERT_TRUE(node->children[0]->children['p' - 'a']->children['p' - 'a']->isEndOfWord);
+  CU_ASSERT_EQUAL(node->children[0]->children['p' - 'a']->children['p' - 'a']->weight, 3);
+
+  for (int i = 0; i < ALPHABET_SIZE; i++)
+    CU_ASSERT_EQUAL(node->children[0]->children['p' - 'a']->children['p' - 'a']->children[i], NULL);
+}
+
 void add_tests_trie() {
   CU_pSuite suite = CU_add_suite("Trie Tests", NULL, NULL);
 
   CU_add_test(suite, "test getNode()", test_getNode);
+  CU_add_test(suite, "test insert single word", test_insert_single_word);
 }
