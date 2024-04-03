3a69db787f64f8da776dcbd4201a30177a99ba6c
4v0n <navodhk@hotmail.com>
2024-04-03 17:06:40 +0100

    Update word complete help message

--- include/word_completion.h
+++ include/word_completion.h
@@ -6,7 +6,7 @@
 #include <list.h>
 
 #define WORDS_PATH "../resources/words.csv"
-#define WORD_COMPLETE_HELP "HELP: Ctrl-enter = complete | Ctrl-a/d = Select | Ctrl-w/s = Switch Mode"
+#define WORD_COMPLETE_HELP "HELP: Ctrl-enter = complete | Ctrl-a/d = Select | Ctrl-w = Switch Mode"
 #define MAX_PREFIX_LENGTH 5
 
 enum completionModes { PREFIX = 0, FUZZY };
